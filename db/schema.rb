f30e4cc3469d0bd3fcc4ddb808452ae7ef04a519
akadane87 <akadane87@gmail.com>
2016-12-12 19:29:02 -0500

    added reference

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 20161213001638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "colors", force: :cascade do |t|
+    t.string   "favorite"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  add_index "colors", ["user_id"], name: "index_colors_on_user_id", using: :btree
 
   create_table "examples", force: :cascade do |t|
     t.text     "text",       null: false
@@ -36,5 +45,6 @@ ActiveRecord::Schema.define(version: 2) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["token"], name: "index_users_on_token", unique: true, using: :btree
 
+  add_foreign_key "colors", "users"
   add_foreign_key "examples", "users"
 end
